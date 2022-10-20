8b1380ddc586748c57168b45fc6a5a410ca152f2
artyomchudakov <artchudakov@gmail.com>
2022-10-20 11:13:10 +0200

    initial commit
    All progress until 16:00 October 19, 2022 by completing the test at the workplace at Elio

---
+++ index.js
@@ -0,0 +1,54 @@
+const priceDisplay = document.getElementById('price');
+const priceNormallyDisplay = document.getElementById('price-normally');
+const productQuantity = document.getElementById('product-quantity');
+const increaseProductQuantityBtn = document.getElementById('increase-product-quantity');
+const decreaseProductQuantityBtn = document.getElementById('decrease-product-quantity');
+const scrollToTopBtn = document.getElementById('scroll-to-top');
+
+// Weight controller
+const largestWeightBtn = document.getElementById('largest-weight-btn');
+const middleWeightBtn = document.getElementById('middle-weight-btn');
+const lowestWeightBtn = document.getElementById('lowest-weight-btn');
+
+const PRICE_PER_KG_DISCOUNT = Number(
+  document.getElementById('kg-price').innerText.split(',').join('.')
+);
+const PRICE_PER_KG_NORMALLY =
+  Number(priceNormallyDisplay.innerText.trim().split(' ')[0].replace(',', '.')) * 2;
+
+function getWeight(element) {
+  return +element.innerText.trim().split(' ')[0];
+}
+
+function getPricePerWeight(pricePerKG, weight) {
+  return ((pricePerKG * weight) / 1000).toFixed(2);
+}
+
+function showCalculatedPrice(element) {
+  element.preventDefault();
+  const weight = getWeight(element.target);
+  const price = getPricePerWeight(PRICE_PER_KG_DISCOUNT, weight);
+  const priceNormally = getPricePerWeight(PRICE_PER_KG_NORMALLY, weight);
+  priceDisplay.innerText = price + ' €';
+  priceNormallyDisplay.innerText = priceNormally + ' €';
+}
+
+// CONTROLLERS, LISTENERS
+
+increaseProductQuantityBtn.addEventListener('click', () => {
+  const quantityValue = Number(productQuantity.innerText);
+  productQuantity.innerText = String(quantityValue + 1);
+});
+
+decreaseProductQuantityBtn.addEventListener('click', () => {
+  const quantityValue = Number(productQuantity.innerText);
+  productQuantity.innerText = String(quantityValue - 1);
+});
+
+largestWeightBtn.addEventListener('click', showCalculatedPrice);
+middleWeightBtn.addEventListener('click', showCalculatedPrice);
+lowestWeightBtn.addEventListener('click', showCalculatedPrice);
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
