a81fe3e73494b578608e6864da241ea099479dfa
artyomchudakov <artchudakov@gmail.com>
2022-10-24 15:09:40 +0200

    complited Tabs Section

--- index.js
+++ index.js
@@ -52,3 +52,25 @@ lowestWeightBtn.addEventListener('click', showCalculatedPrice);
 scrollToTopBtn.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+// Tabs Section
+const about = document.querySelector('.about');
+about.addEventListener('click', function (e) {
+  const btns = document.querySelectorAll('.tab-btn');
+  const articles = document.querySelectorAll('.content');
+
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove('active');
+    });
+    e.target.classList.add('active');
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove('active');
+    });
+    const element = document.getElementById(id);
+    element.classList.add('active');
+  }
+});
